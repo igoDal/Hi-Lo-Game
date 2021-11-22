196a64216d17b22039e0a212f7c6e5b12b78e139
IgoDal <igortenn@gmail.com>
2021-11-22 11:28:03 +0100

    Add project files.

---
+++ Hi-Lo Game/HiLoGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Hi_Lo_Game
+{
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot()
+        {
+            return pot;
+        }
+
+        public static void Guess(bool higher)
+        {
+            int guessedNumber = random.Next(1, MAXIMUM + 1);
+
+            if ((higher && guessedNumber >= currentNumber) || (!higher && guessedNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong.");
+                pot--;
+            }
+
+            currentNumber = guessedNumber;
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+            {
+                Console.WriteLine($"The number is at least {half}.");
+            }
+            else
+            {
+                Console.WriteLine($"The number is at most {half}.");
+            }
+
+            pot--;
+        }
+    }
+}
